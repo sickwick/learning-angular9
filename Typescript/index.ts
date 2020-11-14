a1ff159bb07ce5afa2feda6196752902571dc368
Mihail Shcherbakov <mihail.shch@gmail.com>
2020-11-14 20:51:30 +0300

    [TYPESCRIPT] Interfaces

--- Typescript/index.ts
+++ Typescript/index.ts
@@ -1,32 +1,16 @@
-class Server {
-    static VERSION = '1.0.3';
-
-    // Объявляем через конструктор
-    // public name: string;
-    // protected ip: number;
-
-    private status: string = "working";
-
-    // constructor(name, ip) {
-    //     this.name = name;
-    //     this.ip = ip;
-    // }
-
-    constructor(public name: string, private ip: number) {
-    }
-
-    turnOn() {
-        status = "working";
-    }
-
-    turnOff() {
-        status = "offline"
-    }
-
-    getStatus(): string {
-        return this.status;
-    }
-
+interface IUser {
+    name: string
+    age: number
+    logInfo: () => void
+    id?: any
+    // id: any
 }
 
-const server: Server = new Server("test", 12);
+const user: IUser = {
+    name: "Tom",
+    age: 25,
+    logInfo() {
+        console.log(this.name + ' ' + this.age);
+    },
+    // id: "qweqw"
+};
