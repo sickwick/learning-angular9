8bc95492648782fd9347e93e1c030880ddf3f0d5
Mihail Shcherbakov <mihail.shch@gmail.com>
2020-11-15 15:00:15 +0300

    [ANGULAR] Односторонний binding

--- angular-basics/src/app/app.component.ts
+++ angular-basics/src/app/app.component.ts
@@ -7,9 +7,17 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-basics';
-  arr = [1,2,3]
+  arr = [1, 2, 3];
   obj = {
     a: 1,
     b: {c: 3}
   };
+  img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/220px-React-icon.svg.png';
+
+  constructor() {
+    setTimeout(() => {
+      console.log('timeout');
+      this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png';
+    }, 5000);
+  }
 }
