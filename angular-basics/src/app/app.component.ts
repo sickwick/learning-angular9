9ea2829ef779f75d2613486de13ec1818ddbcff7
Mihail Shcherbakov <mihail.shch@gmail.com>
2020-11-15 14:41:02 +0300

    [ANGULAR] Интерполяция

--- angular-basics/src/app/app.component.ts
+++ angular-basics/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-basics';
+  arr = [1,2,3]
+  obj = {
+    a: 1,
+    b: {c: 3}
+  };
 }
