950ada9e84b3f0835eaddffdf3ad085a83d541e7
Mihail Shcherbakov <mihail.shch@gmail.com>
2020-11-17 18:59:07 +0300

    [ANGULAR] pipes

--- angular-basics/src/app/app.component.ts
+++ angular-basics/src/app/app.component.ts
@@ -6,19 +6,5 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public array = [1, 1, 2, 3, 5, 8, 13];
-  public obj = [
-    {title: 'Hello', name: 'user', comments: [
-        {name: 'max', text: 'lorem2'},
-        {name: 'max', text: 'lorem1'},
-        {name: 'max', text: 'lorem2'},
-        {name: 'max', text: 'lorem3'}
-      ]},
-    {title: 'WOW', name: 'user', comments: [
-        {name: 'UNG', text: 'lorem2'},
-        {name: 'UNG', text: 'lorem1'},
-        {name: 'UNG', text: 'lorem2'},
-        {name: 'UNG', text: 'lorem3'}
-      ]}
-  ];
+  now: Date = new Date();
 }
