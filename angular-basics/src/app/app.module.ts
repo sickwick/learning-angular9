d6908e6625a2f3ce352d1c65435632c5795f7e70
Mihail Shcherbakov <mihail.shch@gmail.com>
2020-11-17 19:11:25 +0300

    [ANGULAR-COMPONENTS] Создание компонентов

--- angular-basics/src/app/app.module.ts
+++ angular-basics/src/app/app.module.ts
@@ -2,21 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {PostComponent} from './post/post.component';
-import { Post2Component } from './post2/post2.component';
-import {Post3Component} from './post3/post3.component';
-import {FormsModule} from '@angular/forms';
+import { PostFormComponent } from './post-form/post-form.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent,
-    Post2Component,
-    Post3Component
+    PostFormComponent,
+    PostComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
