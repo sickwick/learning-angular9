4b20ad16acc3db0a0cc69ca0fc53cbf0af3c1a96
Mihail Shcherbakov <mihail.shch@gmail.com>
2020-11-17 18:53:53 +0300

    [ANGULAR] ngFor

--- angular-basics/src/app/app.component.ts
+++ angular-basics/src/app/app.component.ts
@@ -6,5 +6,19 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public toogle: any = false;
+  public array = [1, 1, 2, 3, 5, 8, 13];
+  public obj = [
+    {title: 'Hello', name: 'user', comments: [
+        {name: 'max', text: 'lorem2'},
+        {name: 'max', text: 'lorem1'},
+        {name: 'max', text: 'lorem2'},
+        {name: 'max', text: 'lorem3'}
+      ]},
+    {title: 'WOW', name: 'user', comments: [
+        {name: 'UNG', text: 'lorem2'},
+        {name: 'UNG', text: 'lorem1'},
+        {name: 'UNG', text: 'lorem2'},
+        {name: 'UNG', text: 'lorem3'}
+      ]}
+  ];
 }
