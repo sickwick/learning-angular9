31bf871bafa43ac6bd36dd8b60d98fa36c852f7c
Mihail Shcherbakov <mihail.shch@gmail.com>
2020-11-28 20:20:56 +0300

    [ANGULAR-DIRECTIVES] Добавление событий

--- angular-basics/src/app/directives/style.directive.ts
+++ angular-basics/src/app/directives/style.directive.ts
@@ -1,12 +1,24 @@
-import {Directive, ElementRef, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appStyle]'
 })
 export class StyleDirective {
   constructor(private el: ElementRef, private render: Renderer2) {
-    render.setStyle(el.nativeElement, 'color', 'blue')
+    // render.setStyle(el.nativeElement, 'color', 'blue');
     // el.nativeElement.style.color = 'red';
     console.log(el);
+  }
+
+  @HostListener('click', ['$event.target']) click(event: Event): void {
+    console.log(event);
+  }
+
+  @HostListener('mouseenter') enter(): void {
+    this.render.setStyle(this.el.nativeElement, 'color', 'yellow');
+  }
+
+  @HostListener('mouseleave') leave(): void {
+    this.render.setStyle(this.el.nativeElement, 'color', null);
   }
 }
