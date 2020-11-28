f813b7bcdd9e3f4d91b1ac5d88d4bff13c2d2e96
Mihail Shcherbakov <mihail.shch@gmail.com>
2020-11-28 20:47:36 +0300

    [ANGULAR-DIRECTIVES] HostBinding

--- angular-basics/src/app/directives/style.directive.ts
+++ angular-basics/src/app/directives/style.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appStyle]'
@@ -6,7 +6,8 @@ import {Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/co
 export class StyleDirective {
 
   @Input('appStyle') color = 'yellow';
-  @Input() dStyles: {border: string, fontWeight: string, borderRadius: string};
+  // @Input() dStyles: {border: string, fontWeight: string, borderRadius: string};
+  @HostBinding('style.color') elColor = null;
 
   constructor(private el: ElementRef, private render: Renderer2) {
     // render.setStyle(el.nativeElement, 'color', 'blue');
@@ -19,16 +20,18 @@ export class StyleDirective {
   }
 
   @HostListener('mouseenter') enter(): void {
-    this.render.setStyle(this.el.nativeElement, 'color', this.color);
-    this.render.setStyle(this.el.nativeElement, 'fontWeight', this.dStyles.fontWeight);
-    this.render.setStyle(this.el.nativeElement, 'border', this.dStyles.border);
-    this.render.setStyle(this.el.nativeElement, 'borderRadius', this.dStyles.borderRadius);
+    this.elColor = this.color;
+    // this.render.setStyle(this.el.nativeElement, 'color', this.color);
+    // this.render.setStyle(this.el.nativeElement, 'fontWeight', this.dStyles.fontWeight);
+    // this.render.setStyle(this.el.nativeElement, 'border', this.dStyles.border);
+    // this.render.setStyle(this.el.nativeElement, 'borderRadius', this.dStyles.borderRadius);
   }
 
   @HostListener('mouseleave') leave(): void {
-    this.render.setStyle(this.el.nativeElement, 'color', null);
-    this.render.setStyle(this.el.nativeElement, 'fontWeight', null);
-    this.render.setStyle(this.el.nativeElement, 'border', null);
-    this.render.setStyle(this.el.nativeElement, 'borderRadius', null);
+    this.elColor = null;
+    // this.render.setStyle(this.el.nativeElement, 'color', null);
+    // this.render.setStyle(this.el.nativeElement, 'fontWeight', null);
+    // this.render.setStyle(this.el.nativeElement, 'border', null);
+    // this.render.setStyle(this.el.nativeElement, 'borderRadius', null);
   }
 }
