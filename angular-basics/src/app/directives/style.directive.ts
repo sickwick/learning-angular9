b2ba7dc1a337ef9c2a42b855173a2b7bfed78569
Mihail Shcherbakov <mihail.shch@gmail.com>
2020-11-28 11:34:19 +0300

    [ANGULAR-DIRECTIVES] Получение доступа до элемента

--- angular-basics/src/app/directives/style.directive.ts
+++ angular-basics/src/app/directives/style.directive.ts
@@ -1,8 +1,12 @@
-import {Directive} from '@angular/core';
+import {Directive, ElementRef, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appStyle]'
 })
 export class StyleDirective {
-
+  constructor(private el: ElementRef, private render: Renderer2) {
+    render.setStyle(el.nativeElement, 'color', 'blue')
+    // el.nativeElement.style.color = 'red';
+    console.log(el);
+  }
 }
