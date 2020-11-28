7ca51d600c92309321f6d8f91d19c41735afbc2d
Mihail Shcherbakov <mihail.shch@gmail.com>
2020-11-28 21:03:34 +0300

    [ANGULAR-DIRECTIVES] Clean project

--- angular-basics/src/app/app.module.ts
+++ angular-basics/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
-import {StyleDirective} from './directives/style.directive';
-import { IfnotdirectiveDirective } from './directives/ifnotdirective.directive';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    StyleDirective,
-    IfnotdirectiveDirective
+    AppComponent
   ],
   imports: [
     BrowserModule,
