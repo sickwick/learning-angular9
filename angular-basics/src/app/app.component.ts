912f2708c81bb1d0dc3ac8518c8b33c0bf7fec5a
mikel <mihail.shch@gmail.com>
2020-12-26 16:28:23 +0300

    [ANGULAR-PIPES] Clean project

--- angular-basics/src/app/app.component.ts
+++ angular-basics/src/app/app.component.ts
@@ -1,35 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
-
-export interface Post {
-  title: string;
-  text: string;
-}
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-
-  promise: Promise<string> = new Promise<string>(resolve => {
-    setTimeout(() => {
-      resolve('Promise Resolved');
-    }, 4000);
-  });
-
-  date$: Observable<Date> = new Observable<Date>(obs => {
-    setInterval(() => {
-      obs.next(new Date());
-    }, 1000);
-  });
-
-  date: Date = new Date();
-
-  ngOnInit(): void {
-    this.date$.subscribe(date => {
-      this.date = date;
-    });
-  }
+export class AppComponent {
 }
