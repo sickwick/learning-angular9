91022253470350ead3cb30764f2c6f72484c3889
mikel <mihail.shch@gmail.com>
2020-12-26 17:49:19 +0300

    [ANGULAR-Forms+Validation] Валидаторы

--- angular-basics/src/app/app.component.ts
+++ angular-basics/src/app/app.component.ts
@@ -1,24 +1,31 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements  OnInit{
+export class AppComponent implements OnInit {
   form: FormGroup;
 
   ngOnInit(): void {
     this.form = new FormGroup({
-      email: new FormControl(''),
-      password: new FormControl(null)
+      email: new FormControl('', [
+        Validators.email,
+        Validators.required]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6)
+      ])
     });
   }
 
-  submit(): void{
-    console.log('Form sibmited', this.form);
-    const formData = {...this.form.value};
-    console.log('Form data: ', formData);
+  submit(): void {
+    if (this.form.invalid) {
+      console.log('Form sibmited', this.form);
+      const formData = {...this.form.value};
+      console.log('Form data: ', formData);
+    }
   }
 }
