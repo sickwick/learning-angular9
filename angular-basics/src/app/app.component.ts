dc8ccce87f8d2b9e0e325005b4f1f104e991c5b7
mikel <mihail.shch@gmail.com>
2020-12-26 17:34:49 +0300

    [ANGULAR-Forms+Validation] Создание формы

--- angular-basics/src/app/app.component.ts
+++ angular-basics/src/app/app.component.ts
@@ -1,9 +1,19 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements  OnInit{
+  form: FormGroup;
+
+  ngOnInit(): void {
+    this.form = new FormGroup({});
+  }
+
+  submit(): void{
+    console.log('Form sibmited', this.form);
+  }
 }
